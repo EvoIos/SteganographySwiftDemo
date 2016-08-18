52e5068b587d1ab81c5f2aa80d67b4bb4d25ae11
zlanchun <zlanchun@icloud.com>
2016-08-18 23:05:15 +0800

    add project
    first commit

---
+++ SteganographySwiftDemo/SteganographySwiftDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SteganographySwiftDemo
+//
+//  Created by zhenglanchun on 16/8/18.
+//  Copyright © 2016年 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
